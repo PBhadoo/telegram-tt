2a16560399e1f5ccb142cea0c3b1dc4fbf3dfa73
Hash Hackers <87309724+PBhadoo@users.noreply.github.com>
2026-03-08 07:32:54 +0530

    fix: Remove large analysis files from dist to fix CF Pages 25 MiB limit

--- deploy/copy_to_dist.sh
+++ deploy/copy_to_dist.sh
@@ -2,6 +2,10 @@
 
 cp -R ./public/* ${1:-"dist"}
 
+# Remove large analysis files that exceed CF Pages 25 MiB limit
+rm -f ${1:-"dist"}/build-stats.json
+rm -f ${1:-"dist"}/statoscope-report.html
+
 cp ./src/lib/rlottie/rlottie-wasm.wasm ${1:-"dist"}
 
 cp ./node_modules/opus-recorder/dist/decoderWorker.min.wasm ${1:-"dist"}
